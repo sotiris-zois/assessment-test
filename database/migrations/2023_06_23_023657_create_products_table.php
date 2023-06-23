e8aad8ff7b43afc57bd9787c3a00269a4b07082b
sotiriszois <sotiris.zois.dev@gmail.com>
2023-06-23 05:07:13 +0200

    final version

---
+++ database/migrations/2023_06_23_023657_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',10)->index('name_index');
+            $table->string('code')->unique('unique_product_ode');
+            $table->bigInteger('category_id',false,true)->index('category_index');
+            $table->decimal('price',8,2,true);
+            $table->date('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
